740231ba42d88393993d264814c45c15b4970c0e
crader-dev <crader@vt.edu>
2017-09-26 16:15:15 -0400

    Added RxAct drugInteractions API call logic.

--- Client/app/util/rxact-api-caller.js
+++ Client/app/util/rxact-api-caller.js
@@ -1,19 +1,37 @@
 'use strict';
 
-const RXACT_GRAPHQL_URL = location.href.indexOf('/index') + '/graphql';
+const RXACT_GRAPHQL_URL = location.href + '/graphql';
 
+const RXACT_INTERACTION_QUERY_TEMPLATE = `query { \
+    drugInteractions(drugs: $drugList) {
+        description
+        from_drug {
+            name
+        }
+        to_drug {
+            name
+        }
+    }
+ }`;
 
 function getDrugInteractions(drugList, callback) {
-
+    makeRxActInteractionCall({
+        query: RXACT_INTERACTION_QUERY_TEMPLATE,
+        variables: {drugList: drugList}
+    },
+    callback);
 }
 
 function makeRxActInteractionCall(requestJson, callback) {
     let xhttp = new XMLHttpRequest();
+    xhttp.responseType = 'json';
+    xhttp.open("POST", RXACT_GRAPHQL_URL, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader('Accept', 'application/json');
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             callback(xhttp.responseText);
         }
     }
-    xhttp.open("GET", RXACT_GRAPHQL_URL, true);
-    xhttp.send(null);
+    xhttp.send(JSON.stringify(requestJson));
 }
