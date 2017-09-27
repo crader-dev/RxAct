8e7c8bc92df40853d345604e9bfbd49ccba4884c
crader-dev <crader@vt.edu>
2017-09-27 12:44:42 -0400

    Added CSRF token to RxAct API request.

--- Client/app/util/rxact-api-caller.js
+++ Client/app/util/rxact-api-caller.js
@@ -25,13 +25,27 @@ function getDrugInteractions(drugList, callback) {
 function makeRxActInteractionCall(requestJson, callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.responseType = 'json';
-    xhttp.open("POST", RXACT_GRAPHQL_URL, true);
+    xhttp.open('POST', RXACT_GRAPHQL_URL, true);
     xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.setRequestHeader('Accept', 'application/json');
+    xhttp.setRequestHeader('X-CSRFToken', getCSRFToken());
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             callback(xhttp.responseText);
         }
     }
     xhttp.send(JSON.stringify(requestJson));
+}
+
+function getCSRFToken() {
+    let tokenMatch = document.cookie.match(/csrftoken=(.*)/);
+    let token = null;
+    if (tokenMatch.length <= 1) {
+        Logger.error('Could not find RxAct API CSRF token.');
+    } else {
+        // Get first group (the token without the 'csrftoken=' prefix)
+        token = tokenMatch[1];
+    }
+
+    return token;
 }
